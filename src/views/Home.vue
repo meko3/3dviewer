4feb395dcfd1e49271d947c87d19d37dd2a57761
meko3 <hirono.yoneyama@outlook.com>
2020-12-22 00:33:10 +0900

    [feat] add icon link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,9 @@
                 </router-link>
             </Card>
         </div>
+        <div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        </div>
     </div>
 </template>
 
